097f03824a28cf15ad1858be5f371562bf8e8818
lndsilva <lndsilva@hotmail.com>
2024-06-14 16:51:29 -0300

    backend tasks trabalhando com migrations consign

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
+const db = require('./config/db')
+const consign = require('consign')
 
-app.get('/', (req, res)=>{
-    res.status(200).send('Backend...')
-})
+consign()
+    .then('./config/middlewares.js')
+    .into(app)
 
-app.listen(3000, ()=>{
+app.db = db
+
+
+app.listen(3000, () => {
     console.log('Executando backend...')
 })
